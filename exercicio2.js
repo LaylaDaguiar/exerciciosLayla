ddb0540f6eed2c3cb0289776befc3994be2d0aab
LaylaDaguiar <layladaguiar.sl@gmail.com>
2024-08-30 14:27:11 -0300

    Add files via upload

---
+++ exercicio2.js
@@ -0,0 +1,15 @@
+let idadeNumero = 17
+
+let idadeTexto = idadeNumero.toString()
+
+console.log(idadeNumero, typeof idadeNumero)
+console.log(idadeTexto, typeof idadeTexto)
+
+//A primeira deve mostrar o valor e o tipo da primeira variavel
+
+//A segunda deve mostrar o valor e o tipo da segunda variavel
+
+
+
+
+
